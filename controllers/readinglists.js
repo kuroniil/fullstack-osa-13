bbeb6b18aa9a616ae245df893e059196a1aae8ab
kuroniil <niilo.kuronen@helsinki.fi>
2025-08-09 11:32:31 +0300

    13.22

--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -1,6 +1,24 @@
 const router = require('express').Router()
+const jwt = require('jsonwebtoken')
+const { SECRET } = require('../util/config')
 
-const { ReadinglistBlog, Readinglist } = require('../models')
+const { User, ReadinglistBlog, Readinglist } = require('../models')
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    } catch (error) {
+      console.log(error)
+      return res.status(401).json({ error: 'token invalid' })
+    }
+  } else {
+    return res.status(401).json({ error: 'token missing' })
+  }
+
+  next()
+}
 
 router.post('/', async (req, res) => {
   const { blog_id, user_id } = req.body
@@ -14,6 +32,25 @@ router.post('/', async (req, res) => {
   } catch (e) {
     console.log(e)
     res.send('error adding blog to readinglist')
+  }
+})
+
+router.put('/:id', tokenExtractor, async (req, res) => {
+  const user = await User.findByPk(req.decodedToken.id)
+  const readinglist = await Readinglist.findOne({ where: { userId: user.id }})
+  const read = typeof req.body.read == 'boolean' ? req.body.read : null
+  const blogInReadinglist = await ReadinglistBlog.findOne({ where: { 
+    readinglistId: readinglist.id,
+    blogId: req.params.id
+  }})
+  if ((read !== null) && blogInReadinglist) {
+    const readinglist_blog = await ReadinglistBlog.update(
+      { readState: read },
+      { where: { blogId: req.params.id, readinglistId: readinglist.id } }
+    )
+    res.json(readinglist_blog)
+  } else {
+    res.status(400).end()
   }
 })
 
